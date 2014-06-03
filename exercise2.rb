9fe480594e0eb3c07b4b4f75f0863962228dc4af
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-03 14:05:16 -0400

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+
+x = 55
+y = 0.15
+
+puts x * y
+
+
+x = "whats up"
+y = 5
+
+puts x + y.to_s
+
+a = 45628
+b = 7839
+
+puts "#{a * b}"
+
+(true && false) || (false && true) || !(false && false)
+
+true
